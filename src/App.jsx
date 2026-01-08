ee75e9499109d6df0e525402c17fb403ba479eb8
Arnav Sahil <arnavsahil3@gmail.com>
2026-01-08 19:13:02 +0530

    about initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
-import Services from "./components/Services"
+import Services from "./components/Services";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div className="relative overflow-x-hidden">
       <Home></Home>
       <Services></Services>
+      <About></About>
     </div>
   );
 };
